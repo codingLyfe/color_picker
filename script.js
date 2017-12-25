fcfb0991cc7ca418779633832f4fe66917e12ccc
Tristan Bennett <tmbennett2011@msn.com>
2017-12-25 13:05:18 -0700

    set for loop for event listener to clean up code

--- script.js
+++ script.js
@@ -9,17 +9,36 @@ function	initColorPicker() {
 		green: document.getElementById("green"),
 		blue: document.getElementById("blue")
 	};
-	setColorPickerEventListeners(colorBox,rgb);
+	let colorPickers = document.getElementsByClassName("picker");
+	setColorPickerEventListeners(colorBox, rgb, colorPickers);
 }
 
-function setColorPickerEventListeners (colorBox, rgb){
-	rgb.red.addEventListener('change', () => {
+function setColorPickerEventListeners (colorBox, rgb, pickerElements) {
+
+	for(let i = 0; i < pickerElements.length; i++) {
+		pickerElements[i].addEventListener('change', () => {
+			setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
+		});
+	}
+}
+
+/*
+rgb.red.addEventListener('change', () => {
 		console.log("Red Value: ", rgb.red.value)
+		setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
 	});
 	rgb.green.addEventListener('change', () => {
 		console.log("Green Value: ", rgb.green.value)
+		setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
 	});
 	rgb.blue.addEventListener('change', () => {
 		console.log("Blue Value: ", rgb.blue.value)
+		setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
 	});
+}
+*/
+
+function setBoxBGColor (colorBox, red, green, blue) {
+	rgbVal = [red, green, blue].join(',');
+	colorBox.style.backgroundColor = "rgb(" + rgbVal + ")";
 }
